d32fe6bf916d3e67a7864e88968db3c611b768c3
Chris Rayner <crayner@spartaglobal.com>
2024-07-27 10:13:13 +0100

    init springSecurityFilterChain method for site security

--- CarnasaGamesWebsite/src/main/java/com/sparta/cr/carnasagameswebsite/config/SecurityConfig.java
+++ CarnasaGamesWebsite/src/main/java/com/sparta/cr/carnasagameswebsite/config/SecurityConfig.java
@@ -1,11 +1,50 @@
 package com.sparta.cr.carnasagameswebsite.config;
 
+import com.sparta.cr.carnasagameswebsite.service.SiteUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private final SiteUserService siteUserService;
+
+    @Autowired
+    SecurityConfig(SiteUserService siteUserService){
+        this.siteUserService = siteUserService;
+    }
+
+    @Bean
+    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/index","/","/login","/register").permitAll()
+                .requestMatchers("/auth/**").hasAnyRole("USER","ADMIN")
+                .requestMatchers("/secure/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+                .formLogin(login ->
+                        login
+                                .loginPage("/login")
+                                .permitAll()
+                                .failureForwardUrl("/login")
+                                .defaultSuccessUrl("/auth/home",true)
+                )
+                .logout(logout ->
+                        logout
+                                .logoutSuccessUrl("/")
+                                .permitAll())
+                .userDetailsService(siteUserService).build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
